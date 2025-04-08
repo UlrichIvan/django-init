4f6b6dc294d7df69f68a110a987054ac207d160e
UlrichIvan <mtchokos@gmail.com>
2025-04-08 11:49:32 +0200

    first commit

---
+++ app/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USERNAME"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
